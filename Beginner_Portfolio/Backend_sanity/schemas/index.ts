8efe2a67899bb435c05cedc3314ef90d267cced9
DEBASHIS GARAI <debashis1805@gmail.com>
2024-01-18 23:53:19 +0530

    Inittial push for 'Basic Portfolio'

---
+++ Beginner_Portfolio/Backend_sanity/schemas/index.ts
@@ -0,0 +1,12 @@
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+import contact from './contact'
+
+
+
+export const schemaTypes = [testimonials, abouts, brands, experiences, skills, workExperience, works, contact]
---
+++ Beginner_Portfolio/frontend-portfolio/src/components/index.js
@@ -0,0 +1,2 @@
+// to export the folders in the conponent folder
+export { default as Navbar } from "./Navbar/Navbar";
