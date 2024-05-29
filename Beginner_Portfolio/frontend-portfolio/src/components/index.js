5fe2146bbb02bca65239569e42547ed5cb868b83
DEBASHIS GARAI <debashis1805@gmail.com>
2024-05-29 22:40:51 +0530

    spotify clone init

--- Beginner_Portfolio/frontend-portfolio/src/components/index.js
+++
@@ -1,2 +0,0 @@
-// to export the folders in the conponent folder
-export { default as Navbar } from "./Navbar/Navbar";
---
+++ Spotify_UI_Clone/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-7xl bg-red-500 p-5'>Spotify Clone UI</h1>
+    </>
+  )
+}
+
+export default App
